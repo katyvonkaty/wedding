9b4ceb1e56cd7b827f6239795e26a5379aa1db4e
Katie Jordan <kj0203@gmail.com>
2018-03-30 13:17:46 -0400

    updated

---
+++ public/javascripts/main.js
@@ -0,0 +1,68 @@
+    (function() {
+  /**
+   * Video element
+   * @type {HTMLElement}
+   */
+    var video = document.getElementById("my-video");
+    /**
+     * Check if video can play, and play it
+     */
+    video.addEventListener( "canplay", function() {
+      video.play();
+    });
+  })();
+
+$(window).scroll(function(){
+  if($('.navbar').offset().top >550 ){
+    $('.navbar-fixed-top').addClass('top-nav-collapse');
+  } else {
+    $('.navbar-fixed-top').removeClass('top-nav-collapse');
+
+  }
+})
+
+
+$(function(){
+  $('.page-scroll a').bind('click', function(){
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+});
+
+// $('.carousel').carousel()
+
+var $input;
+
+function onInputFocus(event) {
+  var $target = $(event.target);
+  var $parent = $target.parent();
+  $parent.addClass('input--filled');
+};
+
+function onInputBlur(event) {
+  var $target = $(event.target);
+  var $parent = $target.parent();
+
+  if (event.target.value.trim() === '') {
+    $parent.removeClass('input--filled');
+  }
+};
+
+$(document).ready(function() {
+  $input = $('.input__field');
+  
+  // in case there is any value already
+  $input.each(function(){
+    if ($input.val().trim() !== '') {
+      var $parent = $input.parent();
+      $parent.addClass('input--filled');
+    }
+  });
+  
+  $input.on('focus', onInputFocus);
+  $input.on('blur', onInputBlur);
+});
+
