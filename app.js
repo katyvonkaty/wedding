ff025ba716bb0508fb5363bb913c8f494858b2e8
katyvonkaty <kj0203@gmail.com>
2019-03-22 17:03:40 -0400

    whut

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var users = require('./routes/omg');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/omg', omg);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
